d3335c8a79e80d20e72a5f2540fb7bc6b6c99275
Abel Asres <abelasres@gmail.com>
2021-11-29 23:09:14 -0500

    update gameBoard test

--- test/gameBoard.test.js
+++ test/gameBoard.test.js
@@ -14,9 +14,9 @@ describe('deck and board after game started', ()=>{
     test('start game', (done)=>{
         game.startGame();    
         expect(game.board).not.toEqual([]);
-        expect(game.board.length).toBe(10);
+        expect(game.board.length).toBe(31);
         expect(game.board.length).not.toBe(1);
-        expect(game.board[0]).toEqual([-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]);
+        expect(game.board[0]).toEqual([-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]);
         expect(game.deck.length).not.toBe(109);
         expect(game.deck.length).not.toBe(107);
         //expect(game.deck.length).toBe(108 - (game.players.length * 6));
@@ -34,33 +34,33 @@ describe('deck and board after game started', ()=>{
 
     test('place first tile on grid', (done)=>{
         //invalid play
-        let played = game.playFirstTile(55,4,5);
-        expect(game.board[4][4]).toBe(-1);
+        let played = game.playFirstTile(55,15,16);
+        expect(game.board[15][16]).toBe(-1);
         expect(played).toBe(false);
         //valid play
-        played = game.playFirstTile(55,4,4);
-        expect(game.board[4][4]).toBe(55);
+        played = game.playFirstTile(55,15,15);
+        expect(game.board[15][15]).toBe(55);
         expect(played).toBe(true);
         //call again
-        played = game.playFirstTile(54,4,4);
-        expect(game.board[4][4]).toBe(55);
+        played = game.playFirstTile(54,15,15);
+        expect(game.board[15][15]).toBe(55);
         expect(played).toBe(false);
         done();
     })
 
     test('continue to play tiles', (done)=>{
-        let played = game.playNormalTile(54,4,5);
-        expect(game.board[4][4]).toBe(55);
+        let played = game.playNormalTile(54,15,16);
+        expect(game.board[15][15]).toBe(55);
         //play valid tile next door
-        expect(game.board[4][5]).toBe(54);
+        expect(game.board[15][16]).toBe(54);
         expect(played).toBe(true);
         //play tile on coordinate already used
-        played = game.playNormalTile(52,4,5);
-        expect(game.board[4][5]).toBe(54);
+        played = game.playNormalTile(52,15,16);
+        expect(game.board[15][16]).toBe(54);
         expect(played).toBe(false);
-        //play invalid tile next door
-        played = game.playNormalTile(54,4,3);
-        expect(game.board[4][3]).toBe(-1);
+        //play invalid tile next door (tile already played)
+        played = game.playNormalTile(54,15,14);
+        expect(game.board[15][14]).toBe(-1);
         expect(played).toBe(false);
         done();
     })
